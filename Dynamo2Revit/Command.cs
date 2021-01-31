a96bee06481c8783687794a8885145a3d001b890
beamelement <827907398@qq.com>
2021-01-31 17:09:29 +0800

    初始化工具

---
+++ Dynamo2Revit/Command.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Dynamo.Applications;
+
+namespace Dynamo2Revit
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        Result IExternalCommand.Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string Journal_Dynamo_Path = null ;  //一个空的dynamo文件
+
+            //新建一个窗口
+            Window1 window1 = new Window1();
+
+            if (window1.ShowDialog() == true)
+            {
+                //窗口打开并停留，只有点击按键之后，窗口关闭并返回true
+            }
+
+
+            //按键会改变window的属性，通过对属性的循环判断来实现对按键的监测
+            while (!window1.Done)
+            {
+                //选择一个空的dynamo文件
+                if (window1.selected)
+                {
+
+                    //把选择进来的文件的路径输出一下
+                    Journal_Dynamo_Path =  window1.fileName;
+
+
+
+                }
+
+                if (window1.ShowDialog() == true)
+                {
+                    //窗口打开并停留，只有点击按键之后，窗口关闭并返回true
+                }
+
+            }
+
+            //开启Dynamo但是禁止弹出Dynamo对话框
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData();
+            dynamoRevitCommandData.Application = commandData.Application;
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                { Dynamo.Applications.JournalKeys.ShowUiKey, false.ToString() }, // don't show DynamoUI at runtime
+                { Dynamo.Applications.JournalKeys.AutomationModeKey, true.ToString() }, //run journal automatically
+                { Dynamo.Applications.JournalKeys.DynPathKey, Journal_Dynamo_Path }, //run node at this file path
+                { Dynamo.Applications.JournalKeys.DynPathExecuteKey, true.ToString() }, // The journal file can specify if the Dynamo workspace opened from DynPathKey will be executed or not. If we are in automation mode the workspace will be executed regardless of this key.
+                { Dynamo.Applications.JournalKeys.ForceManualRunKey, false.ToString() }, // don't run in manual mode
+                { Dynamo.Applications.JournalKeys.ModelShutDownKey, true.ToString() }
+
+            };
+
+            dynamoRevitCommandData.JournalData = journalData;
+            Result externalCommandResult = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+            return externalCommandResult;
+
+        }
+    }
+}
---
+++ Dynamo2Revit/Window1.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Windows.Forms;
+
+namespace Dynamo2Revit
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public bool Done;
+        public bool selected;
+        public string fileName;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void selection(object sender, RoutedEventArgs e)
+        {
+            selected = true;
+            this.window.Hide();
+
+
+            //打开一个对话框，选取某种类型文件并获取其路径
+            OpenFileDialog d = new OpenFileDialog();
+
+            d.InitialDirectory = @"C:\Users\zyx\Desktop\"; //默认路径
+            d.Filter = "dynamo文件(*.dyn)|*.dyn";//要选择的文件
+            //d.Filter = "所有文件(*.*)|*.*";
+
+            if (d.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                this.fileName = d.FileName;
+            }
+
+        }
+
+        private void DoneClick(object sender, RoutedEventArgs e)
+        {
+            Done = true;
+            DialogResult = true;
+        }
+
+
+
+
+
+
+    }
+}
